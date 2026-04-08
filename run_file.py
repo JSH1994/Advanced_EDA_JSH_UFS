7ba274ea322fca34ce3895eae35d1d90793ca06e
JSH1994 <147087998+JSH1994@users.noreply.github.com>
2026-04-08 11:38:39 +0200

    Update run_file.py

--- run_file.py
+++ run_file.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
-import numpy as np
 from ydata_profiling import ProfileReport
 
 st.set_page_config(page_title="EDA App", layout="wide")
